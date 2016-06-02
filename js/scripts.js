8eb76826a8ea9ff4631c47cc4b3446b8a8fc65ab
Lauren Burnett and Zach Holz <zach.a.holz@gmail.com>
2016-06-01 19:24:32 -0700

    Added front-end logic for subtraction/multiplication/division and html for results to appear.

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,32 @@ var division = function(number1, number2) {
 
 //Front-end logic
 $(document).ready(function() {
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtraction(number1, number2);
+    $("#difference").text(result);
+  });
+  $("form#multiplication").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiplication1").val());
+    var number2 = parseInt($("#multiplication2").val());
+    var result = multiplication(number1, number2);
+    $("#product").text(result);
+  });
+  $("form#division").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#division1").val());
+    var number2 = parseInt($("#division2").val());
+    var result = division(number1, number2);
+    $("#quotient").text(result);
+  });
 });
