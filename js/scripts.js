a121b11d162403c71da4d0be03266e9f8c56e40f
Lauren Burnett and Zach Holz <zach.a.holz@gmail.com>
2016-06-01 18:46:05 -0700

    Cleaned up repetitve functions and separated front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,27 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-
-alert(result);
-
 // subtraction
 var subtraction = function(number1, number2) {
   return number1 - number2;
 };
 
-var difference = subtraction(number1, number2);
-
-alert(difference);
 
 // multiplication
 var multiplication = function(number1, number2) {
   return number1 * number2;
 };
 
-var product = multiplication(number1, number2);
-
-alert(product);
 
 // division
 var division = function(number1, number2) {
   return number1 / number2;
 };
 
-var quotient = division(number1, number2);
+//Front-end logic
+$(document).ready(function() {
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
 
-alert(quotient);
+alert(add(number1, number2));
+});
